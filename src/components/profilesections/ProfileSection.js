6e930082bd777c1ddde210ef623d96783d5d7d10
UrMama <141613642+Mudipas@users.noreply.github.com>
2025-07-05 14:45:42 +0700

    upadate image profile

--- src/components/profilesections/ProfileSection.js
+++ src/components/profilesections/ProfileSection.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import Card from './Card';
-import '../styles/App.css';
+import Card from '../Card';
+import ImageProfile from './ImageProfile';
+import TextProfile from './TextProfile';
+import '../../styles/App.css';
 
 const ProfileSection = ({ isActive }) => (
   <section id="profile" style={{ scrollMarginTop: '90px' }}>
     <Card className={isActive ? 'profile-active-border' : ''}>
+      <div className="profile-content">
+        <div className="profile-left">
+          <ImageProfile />
+        </div>
+        <div className="profile-right">
+          <TextProfile />
+        </div>
+      </div>
       <h2>Profil</h2>
       <p>Saya merupakan fresh graduate dari Universitas Islam Indonesia, lulus pada tahun 2023. Saya memiliki pengetahuan dan kemampuan dalam bidang IT, software komputer, serta berbagai tools terkait. Saya juga mampu bekerja dalam tim dan komunikasi yang efektif untuk mengelola proyek.</p>
       <div className="profile-btn-container">
